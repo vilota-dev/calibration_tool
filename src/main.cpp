ebb5326018ece1751280ac8c6af2790e423b4c6c
Tejas Garrepally <tejas@mac.local>
2023-05-10 11:41:47 +0800

    new gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
+// Basalt headers
+// #include "basalt/calibration/calibration.hpp"
+#include <cereal/types/unordered_map.hpp>
+
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
 // Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
