0c2f212167b7d522de69bd88fa0634866fa68f72
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-06-01 17:44:34 +0800

    parallelized code for detecting checkerboard corners using libcbdetect

--- src/calibration/calibration_helper.h
+++ src/calibration/calibration_helper.h
@@ -2,6 +2,14 @@
 
 #include "calibration/aprilgrid.h"
 #include "io/dataset_io.h"
+#include "libcbdetect/config.h"
+#include "libcbdetect/find_corners.h"
+#include "libcbdetect/image_normalization_and_gradients.h"
+#include "libcbdetect/get_init_location.h"
+#include "libcbdetect/plot_corners.h"
+#include "libcbdetect/filter_corners.h"
+#include "libcbdetect/refine_corners.h"
+#include "libcbdetect/polynomial_fit.h"
 
 #include <spdlog/spdlog.h>
 #include <tbb/concurrent_unordered_map.h>
@@ -77,6 +85,66 @@ namespace basalt {
       spdlog::info("Done detecting corners. Saved them here: {}", path);
     }
 
+    // Added code for checkerboard detection
+    static void detectCheckerboardCorners(const std::shared_ptr<RosbagDataset> &vio_data,
+                                          cbdetect::Params &params) {
+//      std::string path = "/Users/tejas/Developer/vilota-dev/calibration_tool/data/checkerboard_detected_corners.cereal";
+//
+//      std::ifstream is(path, std::ios::binary);
+//
+//      if (is.good()) {
+//        cereal::BinaryInputArchive archive(is);
+//
+//        vio_data->checkerboard_corners.clear();
+////        archive(vio_data->checkerboard_corners);
+//
+//        spdlog::info("Loaded detected corners from: {}", path);
+//        return;
+//      }
+//
+      spdlog::info("No pre-processed detected corners found, started corner detection");
+
+      vio_data->checkerboard_corners.clear();
+//      for (size_t j = 0; j < vio_data->get_image_timestamps().size(); ++j) {
+//      for (size_t j = 0; j < 1; ++j) {
+//        int64_t timestamp_ns = vio_data->get_image_timestamps()[j];
+//        const std::vector<cv::Mat> &img_vec = vio_data->image_data.at(timestamp_ns);
+//
+//        for (size_t i = 0; i < img_vec.size(); i++) {
+//          spdlog::trace("trying detection for timestamp: {}", timestamp_ns);
+//          cbdetect::Corner corners; // will be populated for this image
+//          try {
+//            cbdetect::find_corners(img_vec[i], corners, params);
+//            TimeCamId tcid(timestamp_ns, i);
+//            vio_data->checkerboard_corners.emplace(tcid, corners);
+//            spdlog::trace("detected for timestamp: {}", timestamp_ns);
+//          } catch (const std::exception &e) {
+//            spdlog::error("Exception: {}", e.what());
+//          }
+//        }
+//      }
+// Parallelized code segfaults for some reason, can't seem to pinpoint, but seems like its from within find_corners
+      tbb::parallel_for(
+              tbb::blocked_range<size_t>(0, vio_data->get_image_timestamps().size()),
+              [&](const tbb::blocked_range<size_t> &r) {
+                for (size_t j = r.begin(); j != r.end(); ++j) {
+                  int64_t timestamp_ns = vio_data->get_image_timestamps()[j];
+                  const std::vector<cv::Mat> &img_vec = vio_data->image_data.at(timestamp_ns);
+                  // Instead of getting cv::Mat, we do the conversion here to cv::Mat instead
+
+                  for (size_t i = 0; i < img_vec.size(); i++) {
+                    cbdetect::Corner corners; // will be populated for this image
+                    cbdetect::find_corners(img_vec[i], corners, params);
+                    TimeCamId tcid(timestamp_ns, i);
+                    vio_data->checkerboard_corners.emplace(tcid, corners);
+                    spdlog::trace("detected for timestamp: {}", timestamp_ns);
+
+                  }
+                }
+              });
+      spdlog::info("Done detecting checkerboard corners");
+    }
+
     static void initCamPoses(
             const Calibration<double>::Ptr &calib,
             const Eigen::aligned_vector<Eigen::Vector4d> &aprilgrid_corner_pos_3d,
@@ -123,5 +191,11 @@ namespace cereal {
   template<class Archive>
   void serialize(Archive &ar, basalt::CalibInitPoseData &c) {
     ar(c.T_a_c, c.num_inliers, c.reprojected_corners);
+  }
+
+  template<class Archive>
+  // For checkerboard corners only, from libcbdetect library.
+  void serialize(Archive &ar, cbdetect::Corner &c) {
+    ar(c.p, c.r, c.v1, c.v2, c.v3, c.score);
   }
 }  // namespace cereal
