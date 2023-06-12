9636aaf8e3f23c36d1808a79e19d3ec6113cc15c
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-06-12 17:44:11 +0800

    only display if subscribed, capnproto deps

--- src/gui/widgets/live_view.hpp
+++ src/gui/widgets/live_view.hpp
@@ -10,6 +10,13 @@ void draw_live_view(ImmVision::ImageParams &image_params, std::shared_ptr<std::v
     float childWidth = ImGui::GetWindowWidth() / 3.0f;
     image_params.ImageDisplaySize = cv::Size(childWidth, 0);
 
+    // Check if the cv::Mat is empty
+    if (display_imgs->at(0).empty()) {
+        ImGui::Text("Subscribe to a camera topic to view live images.");
+        ImGui::End();
+        return;
+    }
+
     for (size_t i = 0; i < display_imgs->size(); i++) {
         ImmVision::Image(std::string("cam_") + std::to_string(i), display_imgs->at(i), &image_params);
         ImGui::SameLine();
