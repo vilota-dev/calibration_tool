a7ccaaf086712298bea55a7f38058a1d1a212fe9
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-06-12 12:05:03 +0800

    fixed rosbag writing issue, no more seg fault. buggy when dataset gets large tho

--- include/recorder/dataset.hpp
+++ include/recorder/dataset.hpp
@@ -1,77 +1,78 @@
 #pragma once
 
-#include <rosbag/bag.h>
+#include "spdlog/spdlog.h"
 #include <ecal/msg/capnproto/subscriber.h>
 #include <ecal_camera/CameraFactory.hpp>
-#include "spdlog/spdlog.h"
+#include <rosbag/bag.h>
 
-#include <string>
-#include <thread>
 #include <atomic>
-#include <mutex>
 #include <chrono>
 #include <ctime>
+#include <mutex>
 #include <sstream>
+#include <string>
+#include <thread>
 
 namespace vk {
-  enum class RecordMode {
-    CONTINUOUS=1,
-    SNAPSHOTS=2
-  };
+    enum class RecordMode {
+        CONTINUOUS = 1,
+        SNAPSHOTS = 2
+    };
 
-  class RosbagDatasetRecorder {
-  public:
-    typedef std::shared_ptr<RosbagDatasetRecorder> Ptr;
+    class RosbagDatasetRecorder {
+    public:
+        typedef std::shared_ptr<RosbagDatasetRecorder> Ptr;
 
-    void init(const vk::CameraParams &params, const RecordMode mode) {
-      this->m_mode = mode;
-      this->m_camera = vk::CameraFactory::getCameraHandler();
-      this->m_camera->init(params); // Setup GUI for default params
+        void init(const vk::CameraParams &params, const RecordMode mode) {
+            this->m_mode = mode;
+            this->m_recording = false;
+            this->m_take_snapshot = false;
+            this->m_camera = vk::CameraFactory::getCameraHandler();
+            this->m_camera->init(params);// Setup GUI for default params
 
-      this->m_camera->registerSyncedCameraCallback(std::bind(&RosbagDatasetRecorder::callbackSyncedCameras, this, std::placeholders::_1));
+            this->m_camera->registerSyncedCameraCallback(std::bind(&RosbagDatasetRecorder::callbackSyncedCameras, this, std::placeholders::_1));
 
-      // Set name for bag file and rosbag
-      {
-        std::ostringstream oss;
-        auto now = std::chrono::system_clock::now();
-        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+            // Set name for bag file and rosbag
+            {
+                std::ostringstream oss;
+                auto now = std::chrono::system_clock::now();
+                std::time_t now_c = std::chrono::system_clock::to_time_t(now);
 
-        oss << std::put_time(std::localtime(&now_c), "%Y-%m-%d-%I-%M-%S");
-        std::string mode_type = this->m_mode == RecordMode::SNAPSHOTS ? "snapshot" : "continuous";
+                oss << std::put_time(std::localtime(&now_c), "%Y-%m-%d-%I-%M-%S");
+                std::string mode_type = this->m_mode == RecordMode::SNAPSHOTS ? "snapshot" : "continuous";
 
-        this->m_bag_name = "./" + oss.str() + mode_type + "_recording.bag";
+                this->m_bag_name = oss.str() + mode_type + "_recording.bag";
 
-        this->m_bag = std::make_shared<rosbag::Bag>();
-      }
+                this->m_bag = std::make_shared<rosbag::Bag>();
+                this->m_bag->open(this->m_bag_name, rosbag::bagmode::Write);
+            }
 
-      this->m_initialised = true;
-    }
+            this->m_initialised = true;
+        }
 
-    inline bool is_running() const { return this->m_recording; }
-    inline RecordMode get_mode() const { return this->m_mode; }
-    inline void take_snapshot() { this->m_take_snapshot = true; }
-    inline int get_num_snapshots() { return this->m_num_snapshots; }
+        inline bool is_running() const { return this->m_recording; }
+        inline RecordMode get_mode() const { return this->m_mode; }
+        inline void take_snapshot() { this->m_take_snapshot = true; }
+        inline int get_num_snapshots() { return this->m_num_msgs; }
 
-    void start_record();
-    void stop_record();
+        void start_record();
+        void stop_record();
 
-  private:
-    bool m_initialised; // Set to true after callback is passed
-    RecordMode m_mode;
-    vk::CameraInterface::Ptr m_camera;
+    private:
+        bool m_initialised;// Set to true after callback is passed
+        RecordMode m_mode;
+        vk::CameraInterface::Ptr m_camera;
 
-    std::string m_bag_name;
-    std::shared_ptr<rosbag::Bag> m_bag;
-    std::mutex m_mutex; // Safe access of m_bag
-    std::shared_ptr<std::thread> m_thread_image;
-    std::shared_ptr<std::thread> m_thread_imu;
-    std::atomic<int> m_num_snapshots;
-    std::atomic<bool> m_take_snapshot;
-    std::atomic<bool> m_recording;
+        std::string m_bag_name;
+        std::shared_ptr<rosbag::Bag> m_bag;
+        std::mutex m_mutex;// Safe access of m_bag
+                           //    std::shared_ptr<std::thread> m_thread_image;
+                           //    std::shared_ptr<std::thread> m_thread_imu;
+        std::atomic<int> m_num_msgs;
+        std::atomic<bool> m_take_snapshot;
+        std::atomic<bool> m_recording;
 
-    void callbackSyncedCameras(const std::vector<vk::CameraFrameData::Ptr>& dataVector);
-    void callbackImu(vk::ImuFrameData::Ptr data);
-
-
-  };
-}
+        void callbackSyncedCameras(const std::vector<vk::CameraFrameData::Ptr> &dataVector);
+        void callbackImu(vk::ImuFrameData::Ptr data);
+    };
+}// namespace vk
--- src/recorder/dataset.cpp
+++ src/recorder/dataset.cpp
@@ -1,69 +1,95 @@
 #include "recorder/dataset.hpp"
-#include "spdlog/spdlog.h"
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/image_encodings.h"
+#include "spdlog/spdlog.h"
+
+#include <cstring>
 
 namespace vk {
-  void RosbagDatasetRecorder::callbackSyncedCameras(const std::vector<vk::CameraFrameData::Ptr> &dataVector) {
-    if (!this->m_initialised) {
-      spdlog::warn("Rosbag Dataset Recorder is not initialized yet!");
-      return;
+    void RosbagDatasetRecorder::callbackSyncedCameras(const std::vector<vk::CameraFrameData::Ptr> &dataVector) {
+        if (!this->m_initialised) {
+            spdlog::warn("Rosbag Dataset Recorder is not initialized yet!");
+            return;
+        } else if (!this->m_recording) {
+            return;
+        }
+        std::lock_guard<std::mutex> lock(this->m_mutex);
+        switch (this->m_mode) {
+            case RecordMode::CONTINUOUS: {
+                if (this->m_recording) {
+                    for (auto &frame: dataVector) {
+                        std::string streamName = frame->prefixed_topic;
+
+                        sensor_msgs::ImagePtr writeMsg(new sensor_msgs::Image);
+
+                        uint32_t sec = frame->ts / 1e9;
+                        uint32_t nsec = frame->ts % uint32_t(1e9);
+                        ros::Time timestamp(sec, nsec);
+                        writeMsg->header.stamp = timestamp;
+                        writeMsg->header.seq = frame->seq;
+
+                        writeMsg->encoding = frame->encoding;
+
+                        writeMsg->height = frame->image.rows;
+                        writeMsg->width = frame->image.cols;
+
+                        auto data_size = frame->image.total() * frame->image.elemSize();
+
+                        writeMsg->data.resize(data_size);
+                        std::memcpy(writeMsg->data.data(), frame->image.data, data_size);
+
+                        this->m_bag->write(streamName, timestamp, writeMsg);
+                    }
+                    this->m_num_msgs++;
+                }
+                break;
+            }
+            case RecordMode::SNAPSHOTS: {
+                if (this->m_recording && this->m_take_snapshot) {
+                    for (auto &frame: dataVector) {
+                        std::string streamName = frame->prefixed_topic;
+
+                        sensor_msgs::ImagePtr writeMsg(new sensor_msgs::Image);
+
+                        uint32_t sec = frame->ts / 1e9;
+                        uint32_t nsec = frame->ts % uint32_t(1e9);
+                        ros::Time timestamp(sec, nsec);
+                        writeMsg->header.stamp = timestamp;
+                        writeMsg->header.seq = frame->seq;
+
+                        writeMsg->encoding = frame->encoding;
+
+                        writeMsg->height = frame->image.rows;
+                        writeMsg->width = frame->image.cols;
+
+                        auto data_size = frame->image.total() * frame->image.elemSize();
+
+                        writeMsg->data.resize(data_size);
+                        std::memcpy(writeMsg->data.data(), frame->image.data, data_size);
+
+                        this->m_bag->write(streamName, timestamp, writeMsg);
+                    }
+                    this->m_num_msgs++;
+                    this->m_take_snapshot = false;
+                }
+                break;
+            }
+            default:
+                throw std::runtime_error("Undefined recording mode");
+                break;
+        }
     }
 
-    switch (this->m_mode) {
-      case RecordMode::CONTINUOUS: {
-        if (this->m_recording) {
-          for (auto &frame: dataVector) {
-            sensor_msgs::ImagePtr writeMsg;
-            // Set the writeMsg params here from the
-            writeMsg->height = frame->image.rows;
-            writeMsg->width = frame->image.cols;
-            writeMsg->encoding = frame->encoding;
-            writeMsg->is_bigendian = false; // idk what this is
-            writeMsg->step = frame->image.cols * frame->image.elemSize(); // full row length in bytes
-            writeMsg->data.assign(frame->image.datastart, frame->image.dataend);
+    void RosbagDatasetRecorder::callbackImu(const vk::ImuFrameData::Ptr data) {}
 
-            std::string streamName = frame->prefixed_topic;
-
-            this->m_bag->write(streamName, ros::Time(frame->ts), writeMsg);
-          }
-        }
-      }
-      case RecordMode::SNAPSHOTS: {
-        if (this->m_recording && this->m_take_snapshot) {
-          for (auto &frame: dataVector) {
-            sensor_msgs::ImagePtr writeMsg;
-            writeMsg->height = frame->image.rows;
-            writeMsg->width = frame->image.cols;
-            writeMsg->encoding = frame->encoding;
-            writeMsg->is_bigendian = false; // idk what this is
-            writeMsg->step = frame->image.cols * frame->image.elemSize(); // full row length in bytes
-            writeMsg->data.assign(frame->image.datastart, frame->image.dataend); // copy
-
-            std::string streamName = frame->prefixed_topic;
-
-            this->m_bag->write(streamName, ros::Time(frame->ts), writeMsg);
-          }
-          this->m_num_snapshots++;
-          this->m_take_snapshot = false;
-        }
-      }
-      default:
-        std::runtime_error("Undefined recording mode");
+    void RosbagDatasetRecorder::start_record() {
+        // this->m_bag->open(this->m_bag_name, rosbag::bagmode::Write);
+        this->m_recording = true;
     }
 
-  }
-
-  void RosbagDatasetRecorder::callbackImu(const vk::ImuFrameData::Ptr data) {}
-
-  void RosbagDatasetRecorder::start_record() {
-    this->m_bag->open(this->m_bag_name, rosbag::bagmode::Write);
-    this->m_recording = true;
-  }
-
-  void RosbagDatasetRecorder::stop_record() {
-    this->m_recording = false;
-    this->m_bag->close();
-    spdlog::trace("Closing bag file at {}", this->m_bag_name);
-  }
-}
+    void RosbagDatasetRecorder::stop_record() {
+        this->m_recording = false;
+        this->m_bag->close();
+        spdlog::trace("Closing bag file at {}", this->m_bag_name);
+    }
+}// namespace vk
