7eb8e0956d7f3540f855e6bdce17cfab73816f7e
g-tejas <ttejasgarrepally@gmail.com>
2023-07-26 11:07:07 +0800

    feat: new presets

--- src/recorder/presets.cpp
+++ src/recorder/presets.cpp
@@ -11,6 +11,8 @@ namespace vk {
             presets.emplace_back("VK360 front", std::vector<std::string>{"S0/camb", "S0/camc", "S0/camd"});
             presets.emplace_back("VK360 back", std::vector<std::string>{"S1/camb", "S1/camc", "S1/camd"});
             presets.emplace_back("VK360 (full)", std::vector<std::string>{"S0/camb", "S0/camc", "S0/camd", "S1/camb", "S1/camc", "S1/camd"});
+            presets.emplace_back("Fisheye 360", std::vector<std::string>{"S0/camd", "S1/camd"});
+            presets.emplace_back("WFOV 360", std::vector<std::string>{"S0/camb", "S0/camc", "S1/camb", "S1/camc"});
         }
 
         return presets;
